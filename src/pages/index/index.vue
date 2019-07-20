8127688f47ceb81ae1364ec75018aaecb24a92f4
student <student@hubu.edu.cn>
2019-07-20 16:44:15 +0800

    page

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -53,8 +53,8 @@ export default {
         'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
         'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
       ],
-      indicatorDots: ture,
-      autoplay: ture,
+      indicatorDots: false,
+      autoplay: false,
       interval: 5000,
       duration: 1000
     }
